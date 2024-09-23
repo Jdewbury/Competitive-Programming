b41200c1c3275f793d4c5cf9b2a3f78380c00a19
jdewbury <Jdewbury@uwaterloo.ca>
2024-09-23 14:07:31 -0400

    completed ransom note

--- python/ransom_note.py
+++ python/ransom_note.py
@@ -1,7 +1,19 @@
+""" using count
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         for l in ransomNote:
             if ransomNote.count(l) > magazine.count(l) or not magazine.count(l):
                 return False
 
+        return True
+"""
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for l in ransomNote:
+            if l not in magazine:
+                return False
+            else:
+                magazine = magazine.replace(l, "", 1)
+        
         return True
